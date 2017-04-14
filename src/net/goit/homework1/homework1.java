230bc14f8d9390c7ba8de490e5fa82a7b9adf23f
Vyacheslav Mamedov <barmalejuwka@gmail.com>
2017-04-14 21:43:42 +0300

    Create home work exersise 1

--- src/net/goit/homework1/homework1.java
+++ src/net/goit/homework1/homework1.java
@@ -1,10 +1,19 @@
 package net.goit.homework1;
 
+import java.util.Scanner;
+
 /**
  * Created by asus on 14.04.17.
  */
 public class homework1 {
     public static void main(String[] args) {
+
+        Scanner name = new Scanner(System.in);
+        System.out.print("Enter the name : Va");
+        String _name = name.next();
+
+
+        System.out.println("Hello " + _name+ "!!!");
 
     }
 
